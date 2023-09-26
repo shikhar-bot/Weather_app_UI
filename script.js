80ecac22cf9120d5b98e29b1a08d871e777241ba
Shikhar gupta <84851377+shikhar-bot@users.noreply.github.com>
2023-09-26 12:13:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add an event listener to the "change location" button
+const changeLocationButton = document.getElementById('change-location-button');
+changeLocationButton.addEventListener('click', () => {
+    // Prompt the user to enter a new location
+    const newLocation = prompt('Enter a new location:');
+    if(newLocation !== null){
+        // Update the location display
+        const locationElement = document.querySelector('.location');
+        locationElement.textContent = newLocation;
+    }
+});
+
